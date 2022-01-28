9df99ff43af1580a356128bb3d8737a272a27969
NurayymJeenbekova <nurayym.jeenbekova.kk@gmail.com>
2022-01-28 23:00:43 +0600

    #4

--- main.js
+++ main.js
@@ -17,4 +17,10 @@ let userEmail = "nurayym.jeenbekova.kk@gmail.com";
 let info = `Name: ${userName} <br>
 Email: ${userEmail}`;
 
-document.write(info);
+document.write(info + "<br>");
+
+//4
+let souvenirs = +prompt(`Сколько сувениров?`);
+let trinkets = +prompt(`Сколько безделушек`);
+
+document.write(`Всего: ${75 * souvenirs + 112 * trinkets} грамм`);
