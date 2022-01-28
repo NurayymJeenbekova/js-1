efc3f2f3f452693c3e3b23a1a25ba481aa600ea2
NurayymJeenbekova <nurayym.jeenbekova.kk@gmail.com>
2022-01-28 22:52:44 +0600

    #3

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 //№1 
-//const greet = "Hello World!";
+const greet = "Hello World!";
 
-//document.write(greet);
+document.write(greet + "<br>");
 
 //№2
 let message = "first";
 document.write(message + "<br>"); 
 
 message = "second";
-document.write(message);
+document.write(message + "<br>");
+
+//3
+let userName = "Nurayym";
+let userEmail = "nurayym.jeenbekova.kk@gmail.com";
+
+let info = `Name: ${userName} <br>
+Email: ${userEmail}`;
+
+document.write(info);
