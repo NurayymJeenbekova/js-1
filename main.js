7c920b62e8430b01525a4595f65814d9ebc26df2
NurayymJeenbekova <nurayym.jeenbekova.kk@gmail.com>
2022-01-28 23:11:14 +0600

    #5

--- main.js
+++ main.js
@@ -23,4 +23,22 @@ document.write(info + "<br>");
 let souvenirs = +prompt(`Сколько сувениров?`);
 let trinkets = +prompt(`Сколько безделушек`);
 
-document.write(`Всего: ${75 * souvenirs + 112 * trinkets} грамм`);
+document.write(`Всего: ${75 * souvenirs + 112 * trinkets} грамм <br>`);
+
+//5
+let a = +prompt(`Введите значение а`);
+let b = +prompt(`Введите значение b`);
+
+const plus = a + b;
+const minus = a - b;
+const multiplication = a * b;
+const division = a / b;
+const remainder = a % b;
+const exponentiation = Math.pow(a, b);
+
+document.write(`Сумма: ${plus}4 <br>`);
+document.write(`Разность: ${minus} <br>`);
+document.write(`Произведение: ${multiplication}  <br>`);
+document.write(`Деление: ${division} <br>`);
+document.write(`Остаток: ${remainder} <br>`);
+document.write(`Степень: ${exponentiation} <br>`);
